871a0b3fb1cc37a4f9c56f4fbf8c313e6d675011
Trey <twguest@students.latrobe.edu.au>
2020-02-18 16:16:46 +1100

    commit

--- misc/base.py
+++ misc/base.py
@@ -9,4 +9,13 @@ Created on Mon Feb 17 10:14:41 2020
 import numpy as np
 
 def check_nan(array):
+    
+    """
+    checks if any values in an array are non-numerical (NaN)
+    
+    :param array: numpy array
+    
+    :return bool:
+    """
+    
     return print(np.isnan(array).any())
